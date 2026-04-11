f96cef8b298804499143b99f96c9801c87b60e3c
MarcoFerreira05 <a106857@alunos.uminho.pt>
2026-04-11 14:41:36 +0100

    First version of the syscounter bpf program

---
+++ bpf_programs/syscounter/syscounter.h
@@ -0,0 +1,27 @@
+#ifndef SYSTRACER_H
+#define SYSTRACER_H
+
+#ifndef S_IFMT
+#define S_IFMT  00170000
+#endif
+
+#ifndef S_IFREG
+#define S_IFREG 0100000
+#endif
+
+#define MAX_OPS 6
+#define MAX_PIDS 100
+#define MAX_COUNTER_ENTRIES 1000000
+
+// Define a struct to be used as key for the counter map, which includes the pid, command and the syscall id (op)
+struct counter_key {
+    char command[16];
+    int pid;
+    int op;
+};
+
+struct counter_value {
+	// definir como vais guardar os tempos
+}
+
+#endif
